8adcd8144569c62ab91a623499314a71170f1f4e
Gianella Graneros <gianella2209@gmail.com>
2025-03-13 11:17:03 -0300

    commit 3

---
+++ Practica Introduccion/ejercicio1.py
@@ -0,0 +1,3 @@
+edad = int(input("Ingresá una edad: "))
+print (f"Me faltan {100 - edad} años para cumplir 100.")
+#print (100 - edad)
---
+++ Practica Introduccion/ejercicio2.py
@@ -0,0 +1,3 @@
+grados_celcius= int(input("INGRESE GRADOS CELSIUS PARA CONVERTIR EN GRADOS FARENHEIT: "))
+grados_far = (grados_celcius * 9/5) + 32     # formula = (0 °C × 9/5) + 32 = 32 °F
+print (f'{grados_celcius}  grados celsius corresponden a  {grados_far}  grados farenheit')
